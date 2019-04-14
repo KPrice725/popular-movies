9647767463b411fafd2851a914d320f67ce3a91e
Kevin Mark Price <KPrice725@aol.com>
2019-04-14 15:48:53 -0400

    Initial commit

---
+++ app/src/main/java/com/boxnotfound/popularmovies/MainActivity.java
@@ -0,0 +1,14 @@
+package com.boxnotfound.popularmovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+***REMOVED***
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+***REMOVED***
+***REMOVED***
