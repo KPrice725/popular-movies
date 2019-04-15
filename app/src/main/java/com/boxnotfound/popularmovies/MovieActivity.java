4881429ac34ac1927ee37aabd86083c7f766784f
Kevin Mark Price <KPrice725@aol.com>
2019-04-15 07:00:41 -0400

    refactor: Change MainActivity to MovieActivity

--- app/src/main/java/com/boxnotfound/popularmovies/MovieActivity.java
+++ app/src/main/java/com/boxnotfound/popularmovies/MovieActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MovieActivity extends AppCompatActivity {
 
 ***REMOVED***
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movie);
 ***REMOVED***
 ***REMOVED***
