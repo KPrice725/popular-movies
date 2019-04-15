3e7f81b2532a7381f813da6e4d6d47427ca9e70b
Kevin Mark Price <KPrice725@aol.com>
2019-04-15 08:06:12 -0400

    feat: Implement Movie class model
    Initial implementation of the Movie class model.  This will be the
    primary means for instantiating Movie objects via movie data fetched
    from the TMDB API.

---
+++ app/src/main/java/com/boxnotfound/popularmovies/model/Movie.java
@@ -0,0 +1,79 @@
+package com.boxnotfound.popularmovies.model;
+
+public class Movie {
+
+    private String title;
+    private String originalTitle;
+    private String overview;
+    private String releaseDate;
+    private double userRating;
+    private String posterPath;
+    private String[] genres;
+
+
+    public Movie(String title, String originalTitle, String overview, String releaseDate, double userRating, String posterPath, String[] genres) {
+        this.title = title;
+        this.originalTitle = originalTitle;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.userRating = userRating;
+        this.posterPath = posterPath;
+        this.genres = genres;
+***REMOVED***
+
+    public String getTitle() {
+        return title;
+***REMOVED***
+
+    public void setTitle(String title) {
+        this.title = title;
+***REMOVED***
+
+    public String getOriginalTitle() {
+        return originalTitle;
+***REMOVED***
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+***REMOVED***
+
+    public String getOverview() {
+        return overview;
+***REMOVED***
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+***REMOVED***
+
+    public String getReleaseDate() {
+        return releaseDate;
+***REMOVED***
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+***REMOVED***
+
+    public double getUserRating() {
+        return userRating;
+***REMOVED***
+
+    public void setUserRating(double userRating) {
+        this.userRating = userRating;
+***REMOVED***
+
+    public String getPosterPath() {
+        return posterPath;
+***REMOVED***
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+***REMOVED***
+
+    public String[] getGenres() {
+        return genres;
+***REMOVED***
+
+    public void setGenres(String[] genres) {
+        this.genres = genres;
+***REMOVED***
+***REMOVED***
