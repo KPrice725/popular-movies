ee7088303c0d68c1ddb98b6f0b15c61422d4e02b
Kevin Mark Price <KPrice725@aol.com>
2019-04-16 17:35:42 -0400

    refactor: Rename MainActivity to MovieActivity

--- app/src/main/java/com/boxnotfound/popularmovies/movies/MovieActivity.java
+++ app/src/main/java/com/boxnotfound/popularmovies/movies/MovieActivity.java
@@ -1,8 +1,13 @@
 package com.boxnotfound.popularmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+***REMOVED***
 
 import android.os.Bundle;
+import android.view.View;
+
+***REMOVED***
+import com.boxnotfound.popularmovies.model.source.RemoteMovieDataSource;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+***REMOVED***
+
+    public void testNetwork(View view) {
+        Request request = RemoteMovieDataSource.buildRequest(SortParameters.POPULARITY);
+        RemoteMovieDataSource.getJsonFromRequest(request);
 ***REMOVED***
 ***REMOVED***
