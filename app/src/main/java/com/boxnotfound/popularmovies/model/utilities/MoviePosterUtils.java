81e752b310c2aadedbb565b54975a419017c6c6c
Kevin Mark Price <KPrice725@aol.com>
2019-04-16 09:05:31 -0400

    feat: Add MoviePosterUtils class
    This class will be responsible for providing a complete URL
    for the requested image size from a given posterPath, which
    is pulled from each Movie object's posterPath variable.

---
+++ app/src/main/java/com/boxnotfound/popularmovies/model/utilities/MoviePosterUtils.java
@@ -0,0 +1,28 @@
+package com.boxnotfound.popularmovies.model.utilities;
+
+public class MoviePosterUtils {
+
+    //  Provides the base URL for the TMDB movie poster thumbnail images
+    private static final String TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+
+    // Constants to define the image size, which is appended to the Image Base URL
+    private static final String IMAGE_SIZE_SMALL = "w185";
+    private static final String IMAGE_SIZE_MEDIUM = "w342";
+    private static final String IMAGE_SIZE_LARGE = "w500";
+
+    private MoviePosterUtils() {
+        // prevent instantiation to limit usage to static method calls
+***REMOVED***
+
+    public static String getSmallMoviePosterUrlPath(String posterPath) {
+        return TMDB_IMAGE_BASE_URL + IMAGE_SIZE_SMALL + posterPath;
+***REMOVED***
+
+    public static String getMediumMoviePosterUrlPath(String posterPath) {
+        return TMDB_IMAGE_BASE_URL + IMAGE_SIZE_MEDIUM + posterPath;
+***REMOVED***
+
+    public static String getLargeMoviePosterUrlPath(String posterPath) {
+        return TMDB_IMAGE_BASE_URL + IMAGE_SIZE_LARGE + posterPath;
+***REMOVED***
+***REMOVED***
