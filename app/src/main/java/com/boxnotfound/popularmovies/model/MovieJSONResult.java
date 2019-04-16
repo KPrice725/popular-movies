a45a190bb0364785d4f86e838480624b0c966212
Kevin Mark Price <KPrice725@aol.com>
2019-04-16 09:07:07 -0400

    feat: Add MovieJSONResult class
    This class generates the parent object from the returned JSON object
    when retrieving a page of movies from the TMDB API.  The main feature
    is the results List, which contains a list of movies from the given
    page.  This object is instantiated through deserialization of the
    JSON response via Gson.

---
+++ app/src/main/java/com/boxnotfound/popularmovies/model/MovieJSONResult.java
@@ -0,0 +1,30 @@
+package com.boxnotfound.popularmovies.model;
+
+import com.google.gson.annotations.SerializedName;
+
+***REMOVED***
+
+public class MovieJSONResult {
+
+***REMOVED***
+        The MovieNetworkUtils calls to retrieve popular movie data will be initially deserialized
+        through this class via Gson, as per the TMDB returned JSON fields.
+***REMOVED***
+    @SerializedName("page") private int page;
+
+    @SerializedName("total_pages") private int totalPages;
+
+    @SerializedName("results") private List<Movie> results = null;
+
+    public List<Movie> getResults() {
+        return results;
+***REMOVED***
+
+    public int getTotalPages() {
+        return totalPages;
+***REMOVED***
+
+    public int getPage() {
+        return page;
+***REMOVED***
+***REMOVED***
