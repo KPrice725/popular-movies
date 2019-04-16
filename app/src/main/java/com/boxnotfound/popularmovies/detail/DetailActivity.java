f5e5ceb7c168213fafc31279cfeb48ba4502e0f7
Kevin Mark Price <KPrice725@aol.com>
2019-04-16 17:53:08 -0400

    feat: Add DetailActivity class and layout
    This will be the launched activity when a user selects a
    movie thumbnail in the MovieActivity class.

---
+++ app/src/main/java/com/boxnotfound/popularmovies/detail/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.boxnotfound.popularmovies.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+***REMOVED***
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+***REMOVED***
+***REMOVED***
