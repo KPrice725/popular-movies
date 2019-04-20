d23472abd6b4d4b38578cea659ce86eacbbad957
Kevin Mark Price <KPrice725@aol.com>
2019-04-20 14:49:57 -0400

    test: Initial test implementation of the DetailActivity
    Receive Bundle from MovieActivity and ensure that it contains
    the correct movie ID long value.

--- app/src/main/java/com/boxnotfound/popularmovies/detail/DetailActivity.java
+++ app/src/main/java/com/boxnotfound/popularmovies/detail/DetailActivity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.boxnotfound.popularmovies.R;
+import com.boxnotfound.popularmovies.movies.MovieActivity;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = DetailActivity.class.getSimpleName();
+    private long movieId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            movieId = bundle.getLong(MovieActivity.EXTRA_MOVIE_ID);
+        }
+
     }
 }
