58a5c9645d23a7a513cf9d913eb75031a91133b9
Kevin Mark Price <KPrice725@aol.com>
2019-04-22 14:51:42 -0400

    feat: Initial DetailContract implementation
    Add DetailContract, which defines the responsibilities for the
    DetailActivity View and Presenter components.

---
+++ app/src/main/java/com/boxnotfound/popularmovies/detail/DetailContract.java
@@ -0,0 +1,38 @@
+package com.boxnotfound.popularmovies.detail;
+
+import com.boxnotfound.popularmovies.BasePresenter;
+import com.boxnotfound.popularmovies.BaseView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+
+public interface DetailContract {
+
+    interface Presenter extends BasePresenter {
+
+        void loadMovie(final long movieId);
+
+    }
+
+    interface View extends BaseView<Presenter> {
+
+        void displayLoadingIndicator(final boolean isLoading);
+
+        void displayMovieTitle(@NonNull final String movieTitle);
+
+        void displayMovieOriginalTitle(@NonNull final String movieOriginalTitle);
+
+        void displayMovieOverview(@NonNull final String movieOverview);
+
+        void displayMovieReleaseDate(@NonNull final String movieReleaseDate);
+
+        void displayMovieUserRating(final double movieUserRating);
+
+        void displayMovieGenres(@NonNull final List<String> movieGenres);
+
+        void displayMovieBackdropPoster(@NonNull final String backdropPosterPath);
+
+        void displayNoMovieDetails();
+    }
+}
