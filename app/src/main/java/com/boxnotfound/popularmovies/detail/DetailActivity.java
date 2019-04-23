4124be1683bc05001ff0eb11123fd7ba69c8c37a
Kevin Mark Price <KPrice725@aol.com>
2019-04-23 14:07:51 -0400

    feat: Implement DetailContract.View methods

--- app/src/main/java/com/boxnotfound/popularmovies/detail/DetailActivity.java
+++ app/src/main/java/com/boxnotfound/popularmovies/detail/DetailActivity.java
@@ -1,15 +1,25 @@
 package com.boxnotfound.popularmovies.detail;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.boxnotfound.popularmovies.R;
+import com.boxnotfound.popularmovies.model.source.GenreRepository;
+import com.boxnotfound.popularmovies.model.source.MovieRepository;
+import com.boxnotfound.popularmovies.model.source.RemoteGenreDataSource;
+import com.boxnotfound.popularmovies.model.source.RemoteMovieDataSource;
 import com.boxnotfound.popularmovies.movies.MovieActivity;
 
-public class DetailActivity extends AppCompatActivity {
+import java.util.List;
+
+public class DetailActivity extends AppCompatActivity implements DetailContract.View {
 
     private static final String LOG_TAG = DetailActivity.class.getSimpleName();
+
+    private DetailContract.Presenter detailPresenter;
+
     private long movieId;
 
     @Override
@@ -22,5 +32,64 @@ public class DetailActivity extends AppCompatActivity {
             movieId = bundle.getLong(MovieActivity.EXTRA_MOVIE_ID);
         }
 
+        detailPresenter = new DetailPresenter(MovieRepository.getInstance(RemoteMovieDataSource.getInstance()),
+                GenreRepository.getInstance(RemoteGenreDataSource.getInstance()), this, movieId);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        detailPresenter.start();
+    }
+
+    @Override
+    public void displayLoadingIndicator(boolean isLoading) {
+
+    }
+
+    @Override
+    public void displayMovieTitle(@NonNull String movieTitle) {
+
+    }
+
+    @Override
+    public void displayMovieOriginalTitle(@NonNull String movieOriginalTitle) {
+
+    }
+
+    @Override
+    public void displayMovieOverview(@NonNull String movieOverview) {
+
+    }
+
+    @Override
+    public void displayMovieReleaseDate(@NonNull String movieReleaseDate) {
+
+    }
+
+    @Override
+    public void displayMovieUserRating(double movieUserRating) {
+
+    }
+
+    @Override
+    public void displayMovieGenres(@NonNull List<String> movieGenres) {
+
+    }
+
+    @Override
+    public void displayMovieBackdropPoster(@NonNull String backdropPosterPath) {
+
+    }
+
+    @Override
+    public void displayNoMovieDetails() {
+
+    }
+
+    @Override
+    public void setPresenter(DetailContract.Presenter presenter) {
+        detailPresenter = presenter;
     }
 }
