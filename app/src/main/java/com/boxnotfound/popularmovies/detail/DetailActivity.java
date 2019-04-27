d18f628e52dee817922ed569a64877d310431085
Kevin Mark Price <KPrice725@aol.com>
2019-04-26 20:44:11 -0400

    refactor: Set home back button to finish()
    Override onOptionsItemSelected to set user clicks of the back arrow
    home button to call finish() instead of the superclass implementation.
    This destroys the DetailActivity and returns the user to the
    MovieActivity grid position that they left off at.  Without this,
    the MovieActivity starts back at the top, which is not desirable,
    especially if the grid list gets long.

--- app/src/main/java/com/boxnotfound/popularmovies/detail/DetailActivity.java
+++ app/src/main/java/com/boxnotfound/popularmovies/detail/DetailActivity.java
@@ -11,6 +11,7 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -98,6 +99,15 @@ public class DetailActivity extends AppCompatActivity implements DetailContract.
     protected void onResume() {
         super.onResume();
         detailPresenter.start();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
