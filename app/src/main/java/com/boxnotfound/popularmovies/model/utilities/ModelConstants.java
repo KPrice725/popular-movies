845a2c31c8cacadd852dc70b0cfc6b3619f9748b
Kevin Mark Price <KPrice725@aol.com>
2019-06-16 14:24:22 -0400

    refactor: Add base URLs for Review and Video network calls

--- app/src/main/java/com/boxnotfound/popularmovies/model/utilities/ModelConstants.java
+++ app/src/main/java/com/boxnotfound/popularmovies/model/utilities/ModelConstants.java
@@ -13,6 +13,12 @@ public class ModelConstants {
     public static final String TMDB_API_MOST_POPULAR_URL = TMDB_API_GET_MOVIE_URL + "popular";
     public static final String TMDB_API_TOP_RATED_URL = TMDB_API_GET_MOVIE_URL + "top_rated";
 
+    // Provides the base URL for the video list query to get trailers associated with a given movie
+    public static final String TMDB_API_GET_VIDEOS_URL = TMDB_API_GET_MOVIE_URL;
+
+    // Provides the base URL for the review list query to get reviews associated with a given movie
+    public static final String TMDB_API_GET_REVIEWS_URL = TMDB_API_GET_MOVIE_URL;
+
     // API Key Parameter required for all TMDB API GET requests
     public static final String API_KEY_PARAM = "api_key";
 
